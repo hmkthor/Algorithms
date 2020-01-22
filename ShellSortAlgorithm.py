037903d81f498dadb105023f397eca1287c351fc
Hyemin Kim <hmkthor@gmail.com>
2020-01-22 12:55:37 +0900

    modified

--- ShellSortAlgorithm.py
+++ ShellSortAlgorithm.py
@@ -10,18 +10,21 @@ def shell_sort(random_list):
     while h>0:
         for i in range(h):
             start_index = i+h
+           
 
-            temp = random_list[start_index]
-            insert_index = start_index
+            while start_index < len(random_list):
+                temp = random_list[start_index]
+                insert_index = start_index
 
-            while insert_index > h-1 and random_list[insert_index-h] > temp:
-                random_list[insert_index] = random_list[insert_index-h]
-                insert_index = insert_index - h
-            
-            random_list[insert_index] = temp
-            insert_index = insert_index + h
-            
-            h=h//3
+                while insert_index > h-1 and random_list[insert_index-h] > temp:
+                    random_list[insert_index] = random_list[insert_index-h]
+                    insert_index = insert_index - h
+                    
+
+                random_list[insert_index] = temp
+                start_index = insert_index + h
+                
+        h=h//3
 
 if __name__=='__main__':
     list=[]
