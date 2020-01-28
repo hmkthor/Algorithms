b9e670bf55ba12e6ea1f488cb30725906787dcc9
Hyemin Kim <hmkthor@gmail.com>
2020-01-28 21:28:22 +0900

    Stack

---
+++ Stack.py
@@ -0,0 +1,18 @@
+def push(item):
+    stack.append(item)
+
+def pop():
+    return stack.pop()
+
+if __name__ == '__main__':
+    stack = []
+    push(1)
+    push(2)
+    push(3)
+    push(4)
+
+    print("Original stack")
+    print(stack)
+
+    while stack:
+        print("POP > {}".format(pop()))
