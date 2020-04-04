77ceb3fe0988eb84c9b2ae88452cafb8a04c9631
Hyemin Kim <hmkthor@gmail.com>
2020-04-04 11:28:28 +0900

    modified

--- Baekjoon Online Judge/12101.py
+++ Baekjoon Online Judge/12101.py
@@ -25,3 +25,43 @@
 출력
 n을 1, 2, 3의 합으로 나타내는 방법 중에서 사전 순으로 k번째에 오는 것을 출력한다. k번째 오는 식이 없는 경우에는 -1을 출력한다.
 """
+
+def dfs:
+    
+
+
+#n, k 받기 
+n, k = map(int, input().split())
+
+# 각 완성된 식을 저장 할 배열 
+final_arr = []
+
+dfs(0)
+
+"""
+def dfs(s):
+    global result
+    visited[s] = True
+    print(result)
+    if sum(result) == n:
+        total_result.append(result[:])
+        return
+    for next in range(1, 4):
+        if sum(result) < n:
+            result.append(next)
+            dfs(next)
+            result.pop(-1)
+
+# n , k를 맵 형식으로 입력
+n, k = map(int, input().split())
+visited = [False]*(4)
+result = []
+total_result = []
+dfs(0)
+if n == 1 and k == 1:
+    print(1)
+elif len(total_result) > k-1:
+    print("+".join(map(str, total_result[k-1])))
+else:
+    print(-1)
+"""
